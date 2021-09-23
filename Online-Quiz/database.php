e1c6d9a26f3ec541a09098d37142025eb2527e44
shikharongithub <76266033+shikharongithub@users.noreply.github.com>
2021-09-23 12:05:03 +0530

    Update database.php

--- Online-Quiz/database.php
+++ Online-Quiz/database.php
@@ -1,4 +1,4 @@
 <?php
-$con= new mysqli('localhost','root','','exam')or die("Could not connect to mysql".mysqli_error($con));
+$con= new mysqli('sql6.freemysqlhosting.net','','sql6439383','sql6439383')or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
